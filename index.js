d8350ddf6fa34fa1f7542231a286876e87eb6f9a
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-01-21 15:06:32 -0600

    feat: add API endpoints

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Application is running at http://localhost:${port}`);
 });
+
+app.post('/execute', (req, res) => {
+  console.log('POST request received.');
+});
+
+app.post('/investigate', (req, res) => {
+  console.log('POST request received.');
+});
+
+app.post('/randomize', (req, res) => {
+  console.log('POST request received.');
+});
