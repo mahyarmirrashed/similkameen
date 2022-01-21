71a7c66f08793e2a5d3134b10be84df5eb9b7f3b
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-01-21 17:35:37 -0600

    style: format with prettier

--- client/src/App.js
+++ client/src/App.js
@@ -84,42 +84,54 @@ const App = () => {
         <div className="side">
           <h3>Welcome</h3>
           <span>
-            This tool was created by team Similkameen for the 2022 Western Engineering Conference. Read more about the diagrams in the about section and enter data in the user entry section to see the tool in action.
+            This tool was created by team Similkameen for the 2022 Western
+            Engineering Conference. Read more about the diagrams in the about
+            section and enter data in the user entry section to see the tool in
+            action.
           </span>
         </div>
         <div className="centerpanel">
-          <div className='center status_msg'>
+          <div className="center status_msg">
             <strong>Initial configuration</strong>
           </div>
           <div className="box_container" id="input_box_container">
             {inputGrid.map((nums, X) => (
-              <div className = 'row' key={X}>
+              <div className="row" key={X}>
                 {nums.map((nums, Y) => (
-                  <div className={inputGrid[X][Y] === 0 ?'cell':'cell toggled'} key={X*100+Y}/>
+                  <div
+                    className={inputGrid[X][Y] === 0 ? 'cell' : 'cell toggled'}
+                    key={X * 100 + Y}
+                  />
                 ))}
               </div>
             ))}
           </div>
-          <div className='center status_msg'>
+          <div className="center status_msg">
             <strong>Randomized configuration</strong>
           </div>
           <div className="box_container" id="randomized_box_container">
             {randomGrid.map((nums, X) => (
-              <div className = 'row' key={X}>
+              <div className="row" key={X}>
                 {nums.map((nums, Y) => (
-                  <div className={randomGrid[X][Y] === 0 ?'cell':'cell toggled'} key={X*100+Y}/>
+                  <div
+                    className={randomGrid[X][Y] === 0 ? 'cell' : 'cell toggled'}
+                    key={X * 100 + Y}
+                  />
                 ))}
               </div>
             ))}
           </div>
-          <div className='center status_msg'>
+          <div className="center status_msg">
             <strong>Final configuration</strong>
           </div>
           <div className="box_container" id="input_box_container">
             {outputGrid.map((nums, X) => (
-              <div className = 'row' key={X}>
+              <div className="row" key={X}>
                 {nums.map((nums, Y) => (
-                  <div className={outputGrid[X][Y] === 0 ?'cell':'cell toggled'} key={X*100+Y}/>
+                  <div
+                    className={outputGrid[X][Y] === 0 ? 'cell' : 'cell toggled'}
+                    key={X * 100 + Y}
+                  />
                 ))}
               </div>
             ))}
@@ -128,13 +140,23 @@ const App = () => {
         <div className="side">
           <h3>About</h3>
           <span>
-            When a user enters a valid input state and process configuration, Three diagrams will be shown:
+            When a user enters a valid input state and process configuration,
+            Three diagrams will be shown:
           </span>
           <br></br>
           <ul>
-            <li>The first diagram is the inital configuration that the user inputs. </li>
-            <li>The second diagram is a randomized configuration of the initial configuration.</li>
-            <li>The third diagram is the final configuration, along with the steps needed to turn the randomized configuration into the final one.</li>
+            <li>
+              The first diagram is the inital configuration that the user
+              inputs.{' '}
+            </li>
+            <li>
+              The second diagram is a randomized configuration of the initial
+              configuration.
+            </li>
+            <li>
+              The third diagram is the final configuration, along with the steps
+              needed to turn the randomized configuration into the final one.
+            </li>
           </ul>
         </div>
       </div>
@@ -143,7 +165,10 @@ const App = () => {
           <div className="center status_msg">
             <h3>User Entry</h3>
           </div>
-          <span className="center status_msg">To use the crane tool, input the initial configuration and process configuration as numbers separated by a space.</span>
+          <span className="center status_msg">
+            To use the crane tool, input the initial configuration and process
+            configuration as numbers separated by a space.
+          </span>
           <div className="center">
             <label htmlFor="input_config">Input Configuration:</label>
             <input
